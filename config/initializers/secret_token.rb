c5181a07c828029e37c64cf73324e5b26fae0f91
Valeriy Pazyuk <mr.valer00n@gmail.com>
2012-09-04 22:00:21 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vwkonfigurator::Application.config.secret_token = 'a58aba6395c7ee0d0270d83477008f0ecba464a5737f30f85e6bbc3c45ae06a6723ebe0e6e8317e0ac1b3ccb8695f6734dc2f7707c4d3ea82c17f24d4cd4e16d'
